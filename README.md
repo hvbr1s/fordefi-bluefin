22bccd1bcdd9c5b5f6670489fd2e9c5bb45e8c56
Dan Jaheny <danjaheny@mac.home>
2025-03-12 13:10:39 +0100

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+Exploring SUI
+Docs -> https://sdk.mystenlabs.com/typescript
+Building tx -> https://sdk.mystenlabs.com/typescript/transaction-building/basics
+More docs -> https://bluefin-exchange.readme.io/reference/spot-api-sdk-getting-started
---
+++ src/api_request/form_request.ts
@@ -0,0 +1,18 @@
+export async function formRequest(vault_id: string, signatureDataB64: any){
+
+    const requestJson =  {
+        vault_id: vault_id,
+        note: 'Testing SUI',
+        signer_type: 'api_signer',
+        sign_mode: 'auto',
+        type: "sui_transaction",
+        details: {
+          type: "sui_binary_canonical_serialization",
+          chain: "sui_mainnet",
+          data: signatureDataB64,
+        },
+        wait_for_state: 'signed'
+      }
+
+    return requestJson
+}
